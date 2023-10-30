fad85ea0197ee665c1bc275a08795ce3db388328
CMickus <mickuscolin@gmail.com>
2023-10-30 10:56:20 +0100

    base du burger menu

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Footer.module.css';
+import logogit from '../images/github.png';
+import logolinkedin from '../images/linkedin.png'
+
+export function Footer() {
+    return (
+        <footer className={style.footerstyle}>
+            <div>
+                <a href="https://www.linkedin.com/in/colin-mickus-845558260/"><img src={logolinkedin} alt='logo de linkedin' className={style.footerlink}/></a>
+                <a href="https://github.com/CMickus"><img src={logogit} alt='logo de github' className={style.footerlink} /></a>
+            </div>
+            <div>
+                <p>mickuscolin@gmail.com</p>
+                <p>Colin Mickus</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer
