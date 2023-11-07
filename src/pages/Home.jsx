748a73ca4b8a48dfc3be8f0ec630743617e48d6d
CMickus <mickuscolin@gmail.com>
2023-11-07 09:00:28 +0100

    ajout general need debug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,32 +7,43 @@ import style from './Home.module.css'
 
 export function Home(){
     console.log(contentlist.fr.skills)
+    const langParam = fr
+    if (URLSearchParams.has(en)){
+        langParam = en
+    }
     return(
         <>
+        <Slideshow prop={contentlist.screenshots}/>
         <div className={style.explanation}>
-            <h1 className={style.explatitle}>{contentlist.fr.content.title}</h1>
-            <p>{contentlist.fr.content.text}</p>
-            <p>{contentlist.fr.content.bonus}</p>
+            <h1 className={style.explatitle}>{contentlist.${langParam}.content.title}</h1>
+            <p>{contentlist.${langParam}.content.text}</p>
+            <p>{contentlist.${langParam}.content.bonus}</p>
         </div>
         <div className={style.bloclists}>
             <div>
                 <h2>Skill techniques</h2>
-            {contentlist.fr.skills.map((elem) =>
+            {contentlist.${langParam}.skills.map((elem) =>
             <Skill props={elem}/>)}
             </div>
             <div>
                 <h2>Skill linguistique</h2>
-            {contentlist.fr.langskills.map((elem) =>
+            {contentlist.${langParam}.langskills.map((elem) =>
             <Skill props={elem}/>)}
             </div>
         </div>
         <div>
-            {contentlist.fr.diplomes.map((elem) =>
+            {contentlist.${langParam}.diplomes.map((elem) =>
             <SimpleContent props={elem}/>)}
         </div>
         <div>
-            {contentlist.fr.experiance.map((elem) =>
+            {contentlist.${langParam}.experiance.map((elem) =>
             <SimpleContent props={elem}/>)}
+        </div>
+        <div>
+            {contentlist.${langParam}.skills.map((elem) =>
+            <HiddenText info={elem.props.infos} fixe={elem.props.skill}/>)}
+            {contentlist.langParam.langskillsskills.map((elem) =>
+            <HiddenText info={elem.props.infos} fixe={elem.props.skill}/>)}
         </div>
         </>
     )
